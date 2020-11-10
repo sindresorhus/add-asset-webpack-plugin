78311a33af667d21da4cb3048823328ea05b7fd5
silverwind <me@silverwind.io>
2020-11-10 18:34:14 +0100

    Emit string assets only once
    Fixes: https://github.com/sindresorhus/add-asset-webpack-plugin/issues/4

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 const {Compilation, sources: {RawSource}} = require('webpack');
 
+const tapOptions = {
+	name: 'AddAssetPlugin',
+	stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS
+};
+
 module.exports = class AddAssetPlugin {
 	constructor(filePath, source) {
 		this.filePath = filePath;
@@ -10,11 +15,19 @@ module.exports = class AddAssetPlugin {
 
 	apply(compiler) {
 		compiler.hooks.compilation.tap('AddAssetPlugin', compilation => {
-			compilation.hooks.processAssets.tapPromise({
-				name: 'AddAssetPlugin',
-				stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS
-			}, async () => {
-				const source = typeof this.source === 'string' ? this.source : await this.source(compilation);
+			compilation.hooks.processAssets.tapPromise(tapOptions, async () => {
+				let source;
+				if (typeof this.source === 'string') {
+					if (compilation.getAsset(this.filePath)) {
+						// Skip emitting the asset again because it's immutable
+						return;
+					}
+
+					source = this.source;
+				} else {
+					source = await this.source(compilation);
+				}
+
 				compilation.emitAsset(this.filePath, new RawSource(source));
 			});
 		});
